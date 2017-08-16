7f67e4d6e0d93b921318796c306147f3c77aab0b
deb_narayan <debnarayan.cs@gmail.com>
2017-08-16 18:55:19 +0530

    HotServer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,40 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {SQLite, SQLiteObject} from "@ionic-native/sqlite";
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = 'Home';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform,
+              statusBar: StatusBar,
+              splashScreen: SplashScreen,
+              sqlite: SQLite) {
     platform.ready().then(() => {
+        sqlite.create({
+            name: "data.db",
+            location: "default",
+            createFromLocation: 1
+        }).then((db: SQLiteObject) => {
+            console.log(db);
+            db.executeSql("CREATE TABLE IF NOT EXISTS sells (id INTEGER PRIMARY KEY AUTOINCREMENT, customer_name VARCHAR(50), product_brand VARCHAR(30), product_volume VARCHAR(10), product_quantity NUMBER(4), paid_amount NUMBER(15), entry_datetime DATE)", {}).then((data) => {
+                // '(id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+                // 'customer_name VARCHAR(50), ' +
+                // 'product_brand VARCHAR(30), ' +
+                // 'product_volume VARCHAR(10), ' +
+                // 'product_quantity NUMBER(4), ' +
+                // 'paid_amount NUMBER(15), ' +
+                // 'entry_datetime DATE)', {}).then((data) => {
+                console.log("TABLE CREATED: ", data);
+            }).catch((error) => {
+                console.error("Unable to execute sql", error);
+            })
+        }).catch((error) => {
+            console.error("Unable to open database", error);
+        });
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,29 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LocalService } from '../providers/local-service/local-service';
+import {HttpModule} from "@angular/http";
+import {SQLite} from "@ionic-native/sqlite";
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
+      HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LocalService,
+      SQLite
   ]
 })
 export class AppModule {}
